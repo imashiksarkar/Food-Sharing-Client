c91d0b5d18caa965872da4e2a43ad2707a7b576e
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2024-12-30 04:59:48 +0600

    - fix. dashboard nav links

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -5,9 +5,9 @@ const Dashboard = () => {
     <section className='dashboard h-full'>
       <div className='h-full grid grid-cols-1 md:grid-cols-[1fr_4fr]'>
         <aside className='p-4 h-full  rounded-md border-r-2'>
-          <ul className='flex flex-col gap-2 [&>li>a]:block [&>li>a]:p-2 [&>li>a]:bg-gray-600 [&>li>a]:rounded-md'>
+          <ul className='flex flex-col gap-2 [&>li>a]:block [&>li>a]:p-2 [&>li>a]:bg-gray-700 [&>li>a]:rounded-md [&>li>a.active]:bg-gray-500'>
             <li>
-              <NavLink to='/dashboard/foods'>
+              <NavLink to='/dashboard/foods' end>
                 My Foods
               </NavLink>
             </li>
