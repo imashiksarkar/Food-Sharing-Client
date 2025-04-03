6d46caf17fdec6424badaec7ec0d607264273ae0
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-04-03 06:58:35 +0600

    - fix. flatten the features

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
-import EndingSooNFoods from '@/features/foods/EndingSooNFoods'
-import FeaturedFoods from '@/features/foods/FeaturedFoods'
-import FoodShowcase from '@/features/foods/FoodShowcase'
-import FoodSlider from '@/features/foods/FoodSlider'
+import EndingSooNFoods from '@/foods/EndingSooNFoods'
+import FeaturedFoods from '@/foods/FeaturedFoods'
+import FoodShowcase from '@/foods/FoodShowcase'
+import FoodSlider from '@/foods/FoodSlider'
 import WhyChooseUs from '@/WhyChooseUs'
 
 const HomePage = () => {
